3f3dd5c02107047767fd6dc6954a74c2206e6692
Vitaliy Bashkir <vabashkir@icloud.com>
2025-08-14 08:15:06 +0300

    pixabay add

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: '51779185-5748d544e2451f70346636fc6',
+        q: query.trim(),
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => {
+      return response.data.hits;
+    });
+}
